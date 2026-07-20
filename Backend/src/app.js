5fcb476039b70fe231220ed6d48f2b061fa6199a
suryansh sharma <ss8737021@gmail.com>
2026-07-20 23:24:12 +0530

    commit changes

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,34 +1,41 @@
-import path from "path";
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
 import cookieParser from "cookie-parser";
-import authRouter from "./routes/auth.route.js";
+import cors from "cors";
 import morgan from "morgan";
-import cors from "cors"
+
+import authRouter from "./routes/auth.route.js";
 import chatRouter from "./routes/chat.router.js";
 
 const app = express();
 
-app.use((req, res, next) => {
-  req.url = req.url.replace(/\/{2,}/g, "/");
-  next();
-});
+// __dirname for ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(morgan("dev"))
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE"],
-}))
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
+
+app.use(morgan("dev"));
 
 app.use("/api/auth", authRouter);
-app.use("/api/chats", chatRouter)
+app.use("/api/chats", chatRouter);
 
-app.use(express.static(path.join(__dirname, '../public')))
+// Serve frontend
+app.use(express.static(path.join(__dirname, "../public")));
 
-app.get(/.*/, (req, res) => {
-    res.sendFile(path.join(__dirname, '../public', 'index.html'))
-})
+// React routes
+app.get("/*splat", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 
 export default app;
